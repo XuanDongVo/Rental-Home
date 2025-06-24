665a019458d53d01c0c7498833a9f5cd3b6b9917
XuanDong <22130043@st.hcmuaf.edu.vn>
2025-06-24 12:02:50 +0700

    update s3 bucket

--- server/src/services/propertyService.ts
+++ server/src/services/propertyService.ts
@@ -6,6 +6,10 @@ import * as propertyRepository from "../repositories/propertyRepository";
 import { Prisma } from "@prisma/client";
 
 const s3Client = new S3Client({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
   region: process.env.AWS_REGION,
 });
 
